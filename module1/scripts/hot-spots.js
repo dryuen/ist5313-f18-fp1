50159289974c636e87dd160f9d740dff9366cf23
Rc <doccr@CryerLaptop>
2018-10-23 16:11:46 -0500

    added content to topic 2 and activities

--- module1/scripts/hot-spots.js
+++ module1/scripts/hot-spots.js
@@ -9,26 +9,21 @@ function showText( number )
 	
 	if( number == 1 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "Don’t let the name fool you-you don’t need to be an actual chef to use this knife! It’s a commonly used knife that is great for chopping herbs, fruits and vegetables.";
+		document.getElementById( "output-text" ).innerHTML = "answer";
 	}
 	else if( number == 2 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "Wondering how you will dissect that huge Thanksgiving bird? Reach for a carving knife and save yourself the hassle. It’s perfect for carving or slicing meats such as turkeys, chickens, roasts, etc., as the name implies.";
+		document.getElementById( "output-text" ).innerHTML = "answer";
 	}
 	else if( number == 3 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "A nifty little knife for peeling fruits and vegetables.";
+		document.getElementById( "output-text" ).innerHTML = "answer";
 	}
 	else if( number == 4 )
 	{
-		document.getElementById( "output-text" ).innerHTML = "Don’t make the mistake of using the wrong knife to cut through that slab of steak. A steak knife, with its sometimes serrated edge, is great for cutting through steaks and other meats.
-";
+		document.getElementById( "output-text" ).innerHTML = "answer";
 	}
-	else if( number == 5 )
-	{
-		document.getElementById( "output-text" ).innerHTML = "If you’ve ever gone to a restaurant where bread or rolls were served as appetizers, chances are you’ve probably used a bread knife before. Bread knives are used to slice through succulent bread.
-";
-	}
+
 }
 
 function hideOutput()
