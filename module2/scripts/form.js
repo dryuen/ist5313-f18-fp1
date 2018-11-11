9a0ee2f43ccb6c241631415f075931df78d30130
rc <doccr@CryerLaptop>
2018-11-10 19:18:28 -0600

    edited activity select tabs

--- module2/scripts/form.js
+++ module2/scripts/form.js
@@ -1,17 +1,9 @@
-var itemInput = document.getElementById("newItem");
-   var itemName = document.getElementById("itemName");
-   var itemAmount= document.getElementById("itemAmount")
-   var submit = document.getElementById("submit");
-   var list = document.getElementById("list");
-   function addItem() {
-     var currentItem = itemInput.value;
-     var listItem = document.createElement("li");
-     listItem.innerHTML = currentItem;
-     list.appendChild(listItem);
-     itemInput.value = "";
-   }
-   submit.onclick = addItem;
-   clear.onclick = function() {
-     list.innerHTML = "";
-     itemInput.value = "";
-   }
+var options = {
+    valueNames: [ "name", "amount","catagory" ]
+item:"<ul> <li><h3 class='name'></h3><p class='amount' + class='catagory'></p></li></ul>"
+};
+var values = [
+	  { name: 'Ground Beef', amount:'1 pound', catagory:'Meat' }
+	  , { name: 'Beans', city:'1 Can', catagory:'Canned Goods' }
+	];
+var groceryList = new List('grocery-list', options,values);
