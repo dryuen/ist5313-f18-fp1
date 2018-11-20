9216b3af4f71c1acbd8b2c11abaa0a78a470fbfb
rc <doccr@CryerLaptop>
2018-11-19 18:31:13 -0600

    edited activity 2 in module 3

--- module3/scripts/activity.js
+++ module3/scripts/activity.js
@@ -35,12 +35,99 @@
 		document.getElementByID("tryAgain").style.visibility="visible";
 	}
 }*/
-$( "input[type=radio][name=question1]" ).on( "change", function() 
+$( "input[type=radio][name=question1a]" ).on( "change", function() 
 	{
 
-	document.getElementById("q1successA").style.visibility="visible";
+	document.getElementById("q1atryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question1b]" ).on( "change", function() 
+	{
+
+	document.getElementById("q1btryAgain").style.visibility="visible";
 });
 
+$( "input[type=radio][name=question1c]" ).on( "change", function() 
+	{
 
+	document.getElementById("q1cSuccess").style.visibility="visible";
+});
 
+$( "input[type=radio][name=question2a]" ).on( "change", function() 
+	{
 
+	document.getElementById("q2aSuccess").style.visibility="visible";
+});
+$( "input[type=radio][name=question2b]" ).on( "change", function() 
+	{
+
+	document.getElementById("q2btryAgain").style.visibility="visible";
+});
+
+$( "input[type=radio][name=question2c]" ).on( "change", function() 
+	{
+
+	document.getElementById("q2ctryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question3a]" ).on( "change", function() 
+	{
+
+	document.getElementById("q3atryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question3b]" ).on( "change", function() 
+	{
+
+	document.getElementById("q3bSuccess").style.visibility="visible";
+});
+
+$( "input[type=radio][name=question3c]" ).on( "change", function() 
+	{
+
+	document.getElementById("q3ctryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question4a]" ).on( "change", function() 
+	{
+
+	document.getElementById("q4aSuccess").style.visibility="visible";
+});
+$( "input[type=radio][name=question4b]" ).on( "change", function() 
+	{
+
+	document.getElementById("q4btryAgain").style.visibility="visible";
+});
+
+$( "input[type=radio][name=question4c]" ).on( "change", function() 
+	{
+
+	document.getElementById("q4ctryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question5a]" ).on( "change", function() 
+	{
+
+	document.getElementById("q5atryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question5b]" ).on( "change", function() 
+	{
+
+	document.getElementById("q5bSuccess").style.visibility="visible";
+});
+
+$( "input[type=radio][name=question5c]" ).on( "change", function() 
+	{
+
+	document.getElementById("q5ctryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question6a]" ).on( "change", function() 
+	{
+
+	document.getElementById("q6atryAgain").style.visibility="visible";
+});
+$( "input[type=radio][name=question6b]" ).on( "change", function() 
+	{
+
+	document.getElementById("q6btryAgain").style.visibility="visible";
+});
+
+$( "input[type=radio][name=question6c]" ).on( "change", function() 
+	{
+	document.getElementById("q6cSuccess").style.visibility="visible";
+});
